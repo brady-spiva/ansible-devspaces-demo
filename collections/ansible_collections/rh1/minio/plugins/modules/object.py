1a343005b01c088dba5ffe302ff41927197f9713
karodrig <karodrig@redhat.com>
2024-01-23 13:58:14 -0500

    updated object module and dev file

--- collections/ansible_collections/rh1/minio/plugins/modules/object.py
+++ collections/ansible_collections/rh1/minio/plugins/modules/object.py
@@ -1,209 +1,272 @@
 #!/usr/bin/python
-
-# Copyright: (c) 2018, Terry Jones <terry.jones@example.org>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-from __future__ import (absolute_import, division, print_function)
+from ansible.module_utils.basic import AnsibleModule
 from minio import Minio
-from minio.error import S3Error
+from minio.commonconfig import CopySource
+from minio.error import (InvalidResponseError, S3Error)
+
 __metaclass__ = type
 
 DOCUMENTATION = r'''
 ---
 module: object
 
-short_description: This is my test module
+short_description: A module for creating and removing buckets in Minio
 
-# If this is part of a collection, you need to use semantic versioning,
-# i.e. the version is of the form "2.5.0" and not "2.4".
 version_added: "1.0.0"
 
-description: This is my longer description explaining my test module.
+description: Ansible custom module for interacting with MinIO to put, get, remove, list, and copy objects.
 
 options:
-    name:
-        description: This is the message to send to the test module.
+    endpoint: 
+        description: The MinIO server endpoint.
         required: true
         type: str
-    new:
-        description:
-            - Control to demo if the result of this module is changed or not.
-            - Parameter description can be a list as well.
+    
+    access_key: 
+        description: The access key for authentication.
+        required: true
+        type: str 
+    
+    secret_key: 
+        description: The secret key for authentication.
+        required: true
+        type: str 
+    
+    mode:
+        description: The desired state of the MinIO object (put, get, remove, list, copy).
+        required: true
+        type: str 
+
+    bucket: 
+        description: The MinIO bucket name
+        required: true
+        type: str 
+
+    object: 
+        description: The MinIO object name
+        required: true
+        type: str 
+
+    src: 
+        description: The content to be uploaded (for put operation).
         required: false
-        type: bool
-# Specify this value according to your collection
-# in format of namespace.collection.doc_fragment_name
-# extends_documentation_fragment:
-#     - my_namespace.my_collection.my_doc_fragment_name
+        type: str 
+
+    src_bucket: 
+        description: The source bucket for copy operation.
+        required: false
+        type: str 
+
+    src_object: description: The source object for copy operation.
+        required: false
+        type: str 
+    
 
 author:
-    - Your Name (@yourGitHubHandle)
+    - Karenna Rodriguez (@ykarennarod)
 '''
 
 EXAMPLES = r'''
-# Pass in a message
-- name: Test with a message
-  my_namespace.my_collection.my_test:
-    name: hello world
+# Upload an object to a bucket
+- name: Upload an object
+  rh1.minio.object:
+    endpoint: {{ endpoint }}
+    access_key: {{ access_key }}
+    secret_ket: {{ secret_key }}
+    bucket: rh1_bucket
+    object: rh1_object
+    mode: put
 
-# pass in a message and have changed true
-- name: Test with a message and changed output
-  my_namespace.my_collection.my_test:
-    name: hello world
-    new: true
+# List objects in a specified bucket
+- name: List objects in rh1_bucket
+  rh1.minio.object:
+    endpoint: {{ endpoint }}
+    access_key: {{ access_key }}
+    secret_ket: {{ secret_key }}
+    bucket: rh1_bucket
+    mode: list
+  register: object_list 
 
-# fail the module
-- name: Test failure of the module
-  my_namespace.my_collection.my_test:
-    name: fail me
+- name: Debug object list 
+  ansible.builtin.debug:
+    var: object_list
+
+# Copy existing object to new object in the same bucket
+- name: Copy rh1_object
+  rh1.minio.object:
+    endpoint: {{ endpoint }}
+    access_key: {{ access_key }}
+    secret_ket: {{ secret_key }}
+    src_bucket: rh1_bucket
+    src_object: rh1_object
+    bucket: rh1_bucket
+    object: new_rh1_object
+    mode: copy
+
 '''
 
 RETURN = r'''
 # These are examples of possible return values, and in general should use other names for return values.
-original_message:
-    description: The original name param that was passed in.
+msg:
+    description: The output msg that the module generates.
     type: str
     returned: always
-    sample: 'hello world'
-message:
-    description: The output message that the test module generates.
-    type: str
-    returned: always
-    sample: 'goodbye'
+    sample: "MinIO object {object} uploaded to {bucket} successfully."
 '''
 
-from ansible.module_utils.basic import AnsibleModule
+def fput_object(module, client, bucket, object, src):
+    """
+    Put MinIO object of unknown size in specified bucket.
 
-def get_object(client, bucket, object):
-    # Get object.
+    Args:
+      - module: Ansible module instance.
+      - client: MinIO client instance.
+      - bucket: Name of the MinIO bucket.
+      - objec: Name of the MinIO object.
+      - src: Content to be uploaded. 
+
+    Returns:
+      - Tuple: (success: bool, msg: str)
+    """
     try:
-        response = client.get_object(bucket, object)
-    # Read data from response.
-    finally:
-        response.close()
-        response.release_conn()
+        client.fput_object(bucket, object, src)
+        return True, f'MinIO object {object} uploaded to {bucket} successfully.'
+    except InvalidResponseError as e: 
+        return False, str(e)
 
-def put_object(client, bucket, object, src):
-    # Put object.
-    # Upload unknown sized data.
-    result = client.put_object(
-        bucket, object, src, length=-1, part_size=10*1024*1024,
-    )
-    print(
-        "created {0} object; etag: {1}, version-id: {2}".format(
-            result.object_name, result.etag, result.version_id,
-        ),
-    )
+def fget_object(module, client, bucket, object, dest):
+    # Get MinIO object from specified bucket.
+    try:
+        client.fget_object(bucket, object, dest)
+        return True, f'MinIO object {object} retrieved successfully.'
+    except InvalidResponseError as e:
+        return False, None, str(e)
 
-def fget_object(client, bucket, object, dest):
-    # Download an object file.
-    client.fget_object(bucket, object, dest)
+def remove_object(module, client, bucket, object):
+    try: 
+        client.remove_object(bucket, object)
+        return True, f'MinIO object {object} removed successfully.'
+    except InvalidResponseError as e:
+        return False, str(e)
 
-def fput_object(client, bucket, object, src):
-    # Upload file 
-    client.fput_object(bucket, object, src)
+def list_object(module, client, bucket):
+    try: 
+        objects = [obj.object_name for obj in client.list_objects(bucket)]
+        return True, f'Existing MinIO objects in {bucket}: {objects}'
+    except InvalidResponseError as e: 
+        return False, str(e)
 
-def remove_object(client, name):
-    client.remove_object(name)
-
-def list_object(client, bucket):
-    objects = client.list_objects(bucket)
-    return objects
+def copy_object(module, client, bucket, object, src_bucket, src_object):
+    try: 
+        client.copy_object(bucket, object, CopySource(src_bucket, src_object))
+        return True, f'MinIO object {src_object} copied to {object} successfully.'
+    except (InvalidResponseError, S3Error) as e: 
+        return False, str(e)
 
 def run_module():
     # define available arguments/parameters a user can pass to the module
     module_args = dict(
         bucket=dict(type='str', required=True),
-        object=dict(type='str', required=True),
-        src=dict(type='str', required=False), #or data
+        object=dict(type='str', required=False),
+        src=dict(type='str', required=False), 
+        src_bucket=dict(type='str', required=False), 
+        src_object=dict(type='str', required=False), 
         dest=dict(type='str', required=False), 
         mode=dict(type='str', choices=['get','put', 'copy', 'list', 'remove', 'stat', 'fget', 'fput'], required=True),
+        access_key=dict(type='str', required=True),
+        secret_key=dict(type='str', required=True),
+        endpoint=dict(type='str', required=True),
     )
-    # seed the result dict in the object
-    # we primarily care about changed and state
-    # changed is if this module effectively modified the target
-    # state will include any data that you want your module to pass back
-    # for consumption, for example, in a subsequent task
+
     result = dict(
         changed=False,
-        message=''
+        msg=""
     )
-    # the AnsibleModule object will be our abstraction working with Ansible
-    # this includes instantiation, a couple of common attr would be the
-    # args/params passed to the execution, as well as if the module
-    # supports check mode
+
     module = AnsibleModule(
         argument_spec=module_args,
-        supports_check_mode=True
+        supports_check_mode=True,
     )
     
     client = Minio(
-        module.params['minio_url'],
+        module.params['endpoint'],
         access_key=module.params['access_key'],
         secret_key=module.params['secret_key'],
     )
 
-    # if the user is working with this module in only check mode we do not
-    # want to make any changes to the environment, just return the current
-    # state with no modifications
     if module.check_mode:
+        result['changed'] = True
         module.exit_json(**result)
 
-    if module.params['mode'] == "get": 
-        get_object(
+    if module.params['mode'] == "remove": 
+        success, msg = remove_object(
+            module=module,
             client=client,
             bucket=module.params['bucket'], 
             object=module.params['object']
         )
-        # changed = True 
-        # result['message'] = "object" + module.params['object'] + "was retrieved successfully." 
-   
-    elif module.params['mode'] == "put": 
-        put_object(
-            client=client,
-            bucket=module.params['bucket'], 
-            object=module.params['object'], 
-            src=module.params['src']
-        )
-
-    elif module.params['mode'] == "remove": 
-        remove_object(
-            client=client,
-            bucket=module.params['bucket'], 
-            object=module.params['object']
-        )
+        result['msg'] = msg
 
     elif module.params['mode'] == "fput": 
-        fput_object(
+        success, msg = fput_object(
+            module=module,
             client=client,
             bucket=module.params['bucket'], 
             object=module.params['object'], 
             src=module.params['src']
         )
+        result['msg'] = msg
     
     elif module.params['mode'] == "fget": 
-        fget_object(
+        success, msg = fget_object(
+            module=module,
             client=client,
             bucket=module.params['bucket'], 
             object=module.params['object'], 
             dest=module.params['dest']
         )
+        result['msg'] = msg
 
-#COPY LIST STAT 
+    elif module.params['mode'] == "list": 
+        success, msg = list_object(
+            module=module,
+            client=client,
+            bucket=module.params['bucket'], 
+        )
+        result['msg'] = msg
+    
+    elif module.params['mode'] == "copy": 
+        success, msg = copy_object(
+            module=module,
+            client=client,
+            bucket=module.params['bucket'], 
+            object=module.params['object'], 
+            src_bucket=module.params['src_bucket'], 
+            src_object=module.params['src_object'], 
+        )
+        result['msg'] = msg
 
-    # during the execution of the module, if there is an exception or a
-    # conditional state that effectively causes a failure, run
-    # AnsibleModule.fail_json() to pass in the message and the result
-    # if module.params['name'] == 'fail me':
-    #     module.fail_json(msg='You requested this to fail', **result)
+    elif module.params['mode'] == "stat": 
+        success, msg = stat_object(
+            module=module,
+            client=client,
+            bucket=module.params['bucket'], 
+            object=module.params['object'], 
+        )
+        result['msg'] = msg
 
-    # in the event of a successful module execution, you will want to
-    # simple AnsibleModule.exit_json(), passing the key/value results
+    # Register success or failure 
+    if success:
+        result['changed'] = True
+    else:
+        result['msg'] = f"Failed to perform MinIO operation: {msg}"
+        module.fail_json(**result)
+        
     module.exit_json(**result)
-
 
 def main():
     run_module()
-
 
 if __name__ == '__main__':
     main()
